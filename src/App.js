470ebfd5031f6dc30260f4110eb88c7e50a62a5a
Agnieszka26 <medrek.agn@yahoo.com>
2021-12-08 13:20:49 +0100

    Add main functionality (face recognition)
    Using clarify API gives ability to face recognition
    Adding dynamically image by user

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
 import React from 'react';
 import './App.css';
+import FaceRecognition from './Components/FaceRecognition/FaceRecognition';
 import Navigation from './Components/Navigation/Navigation';
 import Rank from './Components/Rank/Rank';
 import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
 import Particle from "./Components/Particles/Particles";
+import { Component } from 'react';
+import Clarifai from 'clarifai';
+
+const clarifaiApp = new Clarifai.App({
+ apiKey: '320e122602804d35b58b0e94b53ed72d'
+});
 
 
-const App = () => {
+class App extends Component{
+  constructor(){
+    super();
+    this.state ={
+      input: '',
+      imageUrl: ''
+    }
+  }
+
+  onInputChange = (event) =>{
+    this.setState({input: event.target.value});
+  }
+
+
+  onButtonSubmit = () =>{
+    this.setState({imageUrl : this.state.input})
+    clarifaiApp.models.predict(
+      Clarifai.FACE_DETECT_MODEL,
+      this.state.input
+    )
+    .then(
+      function(response){
+        console.log(response.outputs[0].data.regions[0].region_info.bounding_box)
+      
+      },
+      function(error){
+        
+      }
+    )
+  }
+  
+  render(){
+
+  
   return (
     <div className="App">
         <Particle />
         <Navigation />   
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
         
-    { /*
-    <FaceRecognition />} */}
+ 
+    <FaceRecognition imageUrl={this.state.imageUrl}/>
     </div>
   );
 
   }
 
-
+}
 export default App;
--- src/Components/Particles/Particles.jsx
+++ src/Components/Particles/Particles.jsx
@@ -3,13 +3,13 @@ import './particles.css';
 
 const Particle = () => {
   const particlesInit = (main) => {
-    console.log(main);
+ 
 
     // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
   };
 
   const particlesLoaded = (container) => {
-    console.log(container);
+    
   };
   return (
     <Particles 
